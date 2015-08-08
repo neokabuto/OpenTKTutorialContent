b8efa254523d208f0b2373f06d8c80f9f586a011
Kabuto <neokabuto@gmail.com>
2015-08-08 17:15:24 -0400

    Project file for Tutorial 7

---
+++ OpenTKTutorial7/OpenTKTutorial7/ObjVolume.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace OpenTKTutorial7
+{
+    class ObjVolume : Volume
+    {
+        Vector3[] vertices;
+        Vector3[] colors;
+        Vector2[] texturecoords;
+
+        List<Tuple<int, int, int>> faces = new List<Tuple<int, int, int>>();
+
+        public override int VertCount { get { return vertices.Length; } }
+        public override int IndiceCount { get { return faces.Count * 3; } }
+        public override int ColorDataCount { get { return colors.Length; } }
+
+        /// <summary>
+        /// Get vertices for this object
+        /// </summary>
+        /// <returns></returns>
+        public override Vector3[] GetVerts()
+        {
+            return vertices;
+        }
+
+        /// <summary>
+        /// Get indices to draw this object
+        /// </summary>
+        /// <param name="offset">value to number first vertex in object</param>
+        /// <returns>Array of indices offset to match buffered data</returns>
+        public override int[] GetIndices(int offset = 0)
+        {
+            List<int> temp = new List<int>();
+
+            foreach (var face in faces)
+            {
+                temp.Add(face.Item1 + offset);
+                temp.Add(face.Item2 + offset);
+                temp.Add(face.Item3 + offset);
+            }
+
+            return temp.ToArray();
+        }
+
+        /// <summary>
+        /// Get color data.
+        /// </summary>
+        /// <returns></returns>
+        public override Vector3[] GetColorData()
+        {
+            return colors;
+        }
+
+        /// <summary>
+        /// Get texture coordinates
+        /// </summary>
+        /// <returns></returns>
+        public override Vector2[] GetTextureCoords()
+        {
+            return texturecoords;
+        }
+
+
+        /// <summary>
+        /// Calculates the model matrix from transforms
+        /// </summary>
+        public override void CalculateModelMatrix()
+        {
+            ModelMatrix = Matrix4.Scale(Scale) * Matrix4.CreateRotationX(Rotation.X) * Matrix4.CreateRotationY(Rotation.Y) * Matrix4.CreateRotationZ(Rotation.Z) * Matrix4.CreateTranslation(Position);
+        }
+
+        /// <summary>
+        /// Loads a model from a file.
+        /// </summary>
+        /// <param name="filename">File to load model from</param>
+        /// <returns>ObjVolume of loaded model</returns>
+        public static ObjVolume LoadFromFile(string filename)
+        {
+            ObjVolume obj = new ObjVolume();
+            try
+            {
+                using (StreamReader reader = new StreamReader(new FileStream(filename, FileMode.Open, FileAccess.Read)))
+                {
+                    obj = LoadFromString(reader.ReadToEnd());
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found: {0}", filename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading file: {0}", filename);
+            }
+
+            return obj;
+        }
+
+        public static ObjVolume LoadFromString(string obj)
+        {
+            // Seperate lines from the file
+            List<String> lines = new List<string>(obj.Split('\n'));
+
+            // Lists to hold model data
+            List<Vector3> verts = new List<Vector3>();
+            List<Vector3> colors = new List<Vector3>();
+            List<Vector2> texs = new List<Vector2>();
+            List<Tuple<int, int, int>> faces = new List<Tuple<int, int, int>>();
+
+            // Read file line by line
+            foreach (String line in lines)
+            {
+                if (line.StartsWith("v ")) // Vertex definition
+                {
+                    // Cut off beginning of line
+                    String temp = line.Substring(2);
+
+                    Vector3 vec = new Vector3();
+
+                    if (temp.Count((char c) => c == ' ') == 2) // Check if there's enough elements for a vertex
+                    {
+                        String[] vertparts = temp.Split(' ');
+
+                        // Attempt to parse each part of the vertice
+                        bool success = float.TryParse(vertparts[0], out vec.X);
+                        success |= float.TryParse(vertparts[1], out vec.Y);
+                        success |= float.TryParse(vertparts[2], out vec.Z);
+
+                        // Dummy color/texture coordinates for now
+                        colors.Add(new Vector3((float) Math.Sin(vec.Z), (float) Math.Sin(vec.Z), (float) Math.Sin(vec.Z)));
+                        texs.Add(new Vector2((float) Math.Sin(vec.Z), (float) Math.Sin(vec.Z)));
+
+                        // If any of the parses failed, report the error
+                        if (!success)
+                        {
+                            Console.WriteLine("Error parsing vertex: {0}", line);
+                        }
+                    }
+
+                    verts.Add(vec);
+                }
+                else if (line.StartsWith("f ")) // Face definition
+                {
+                    // Cut off beginning of line
+                    String temp = line.Substring(2);
+
+                    Tuple<int, int, int> face = new Tuple<int, int, int>(0, 0, 0);
+
+                    if (temp.Count((char c) => c == ' ') == 2) // Check if there's enough elements for a face
+                    {
+                        String[] faceparts = temp.Split(' ');
+
+                        int i1, i2, i3;
+
+                        // Attempt to parse each part of the face
+                        bool success = int.TryParse(faceparts[0], out i1);
+                        success |= int.TryParse(faceparts[1], out i2);
+                        success |= int.TryParse(faceparts[2], out i3);
+
+                        // If any of the parses failed, report the error
+                        if (!success)
+                        {
+                            Console.WriteLine("Error parsing face: {0}", line);
+                        }
+                        else
+                        {
+                            // Decrement to get zero-based vertex numbers
+                            face = new Tuple<int, int, int>(i1 - 1, i2 - 1, i3 - 1);
+                            faces.Add(face);
+                        }
+                    }
+                }
+            }
+
+            // Create the ObjVolume
+            ObjVolume vol = new ObjVolume();
+            vol.vertices = verts.ToArray();
+            vol.faces = new List<Tuple<int, int, int>>(faces);
+            vol.colors = colors.ToArray();
+            vol.texturecoords = texs.ToArray();
+
+            return vol;
+        }
+    }
+}
---
+++ OpenTKTutorial7/OpenTKTutorial7/Volume.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK;
+
+namespace OpenTKTutorial7
+{
+    /// <summary>
+    /// An object made up of vertices
+    /// </summary>
+    public abstract class Volume
+    {
+        public Vector3 Position = Vector3.Zero;
+        public Vector3 Rotation = Vector3.Zero;
+        public Vector3 Scale = Vector3.One;
+        
+        public virtual int VertCount { get; set; }
+        public virtual int IndiceCount { get; set; }
+        public virtual int ColorDataCount { get; set; }
+        
+        public Matrix4 ModelMatrix = Matrix4.Identity;
+        public Matrix4 ViewProjectionMatrix = Matrix4.Identity;
+        public Matrix4 ModelViewProjectionMatrix = Matrix4.Identity;
+
+        public abstract Vector3[] GetVerts();
+        public abstract int[] GetIndices(int offset = 0);
+        public abstract Vector3[] GetColorData();
+        public abstract void CalculateModelMatrix();
+
+        public bool IsTextured = false;
+        public int TextureID;
+        public int TextureCoordsCount;
+        public abstract Vector2[] GetTextureCoords();
+    }
+}
